a7eba2281501200298c289c491a773668879047d
SANTIAGONXN <santiagorios.10.guti@gmail.com>
2026-06-25 01:30:57 -0500

    Se agrgo vista nosotros

---
+++ resources/views/nosotros/nostros.blade.php
@@ -0,0 +1,7 @@
+@include('name')
+@extends('layouts.homeAprendiz')
+@section('contenido')
+<a href="ingresAprendiz"><button>Volver al Home</button></a>
+<h5>Hola Bienvenido a la pagina de AULAS MOVILES</h5>
+        <p>Vamos a ver que son las Aulas moviles: </p>
+@endsection
